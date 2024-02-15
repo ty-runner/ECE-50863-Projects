3793e530b28cadb7e6e21a1d10fdb9e658ab54c9
ty-runner <ty.runner02@gmail.com>
2024-02-15 13:54:54 -0500

    working but not passing autograder?

--- Reliable Data Transmission Protocol/starter_code/Student Code/stop_and_go/receiver_stop_and_go.py
+++ Reliable Data Transmission Protocol/starter_code/Student Code/stop_and_go/receiver_stop_and_go.py
@@ -19,10 +19,11 @@ if __name__ == '__main__':
     file_to_send = cfg.get('nodes', 'file_to_send')
     max_packet_size = int(cfg.get('network', 'MAX_PACKET_SIZE'))
     max_packet_size -= 8  # Account for the header size
+    write_location = cfg.get('receiver', 'write_location')
     print('Receiver: Waiting for file contents...')
     #listen for contents of file and sending ACKs
     #open file to write to
-    with open("output.txt", 'wb') as f:
+    with open(write_location, 'wb') as f:
         while True:
             addr, data = recv_monitor.recv(max_packet_size)
             if data == b'':
@@ -33,5 +34,5 @@ if __name__ == '__main__':
             print(f'Receiver: Sending ACK to id {addr}.')
             recv_monitor.send(sender_id, b'ACK')
     f.close()
-    recv_monitor.recv_end('output.txt', sender_id)
+    recv_monitor.recv_end(write_location, sender_id)
     # Exit! Make sure the receiver ends before the sender. send_end will stop the emulator.
