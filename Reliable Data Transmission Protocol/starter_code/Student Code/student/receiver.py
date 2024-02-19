35fb99131752842dee21130f6a81af6cda21ad62
ty-runner <ty.runner02@gmail.com>
2024-02-19 14:25:36 -0500

    messes up on final packets

--- Reliable Data Transmission Protocol/starter_code/Student Code/student/receiver.py
+++ Reliable Data Transmission Protocol/starter_code/Student Code/student/receiver.py
@@ -6,42 +6,42 @@ import time
 import configparser
 
 if __name__ == '__main__':
-    print("Receiver starting up!")
-    config_path = sys.argv[1]
+	print("Receiver starting up!")
+	config_path = sys.argv[1]
 
-    # Initialize sender monitor
-    recv_monitor = Monitor(config_path, 'receiver')
-    
-    # Parse config file
-    cfg = configparser.RawConfigParser(allow_no_value=True)
-    cfg.read(config_path)
-    sender_id = int(cfg.get('sender', 'id'))
-    file_to_send = cfg.get('nodes', 'file_to_send')
-    max_packet_size = int(cfg.get('network', 'MAX_PACKET_SIZE'))
-    max_packet_size -= 12  # Account for the header size
-    write_location = cfg.get('receiver', 'write_location')
-    print('Receiver: Waiting for file contents...')
-    #listen for contents of file and sending ACKs
-    #open file to write to
-    with open(write_location, 'wb') as f:
-        received_count = 0
-        received_list = []
-        data_list = []
+	# Initialize sender monitor
+	recv_monitor = Monitor(config_path, 'receiver')
+	
+	# Parse config file
+	cfg = configparser.RawConfigParser(allow_no_value=True)
+	cfg.read(config_path)
+	sender_id = int(cfg.get('sender', 'id'))
+	file_to_send = cfg.get('nodes', 'file_to_send')
+	max_packet_size = int(cfg.get('network', 'MAX_PACKET_SIZE'))
+	max_packet_size -= 12  # Account for the header size
+	write_location = cfg.get('receiver', 'write_location')
+	print('Receiver: Waiting for file contents...')
+	#listen for contents of file and sending ACKs
+	#open file to write to
+	with open(write_location, 'wb') as f:
+		received_count = 0
+		received_list = []
 		while True:
 			# Receive packet from sender
-			packet = recv_monitor.recv_packet()
+			addr, packet = recv_monitor.recv(max_packet_size)
 			
 			# Check if packet is the end signal
 			if packet == b'':
 				break
 			
 			# Extract sequence number and data from packet
+			print(f'Packet is {packet}.')
 			seq_num = int.from_bytes(packet[:4], byteorder='big')
 			data = packet[4:]
 			
 			# Check if packet is in the window
 			if seq_num >= received_count and seq_num < received_count + max_packet_size:
-			# Check if packet has already been received
+				# Check if packet has already been received
 				if seq_num not in received_list:
 					# Write data to file
 					f.write(data)
@@ -52,9 +52,13 @@ if __name__ == '__main__':
 					
 					# Send ACK to sender
 					ack_packet = seq_num.to_bytes(4, byteorder='big')
-					recv_monitor.send_packet(ack_packet)
+					recv_monitor.send(sender_id, b'ACK' + packet[:4])
+				else:
+					print(f'Receiver: Duplicate packet received with sequence number {seq_num}')
+			else:
+				print(f'Receiver: Packet received with sequence number {seq_num} is not in the window')
 		
 	# Send end signal to sender
-    f.close()
-    recv_monitor.recv_end(write_location, sender_id)
-    # Exit! Make sure the receiver ends before the sender. send_end will stop the emulator.
+	f.close()
+	recv_monitor.recv_end(write_location, sender_id)
+	# Exit! Make sure the receiver ends before the sender. send_end will stop the emulator.
--- Reliable Data Transmission Protocol/starter_code/Student Code/student/sender.py
+++ Reliable Data Transmission Protocol/starter_code/Student Code/student/sender.py
@@ -15,6 +15,7 @@ def create_data_array(file_to_send, max_packet_size):
 		while True:
 			chunk = f.read(max_packet_size)
 			if not chunk:
+				data.append(b'')
 				break
 			header = header_count.to_bytes(4, byteorder='big')
 			data.append(header + chunk)
@@ -39,18 +40,28 @@ if __name__ == '__main__':
 	file_to_send = cfg.get('nodes', 'file_to_send')
 	max_packet_size = int(cfg.get('network', 'MAX_PACKET_SIZE'))
 	max_packet_size -= 12  # Account for the header size
-	window_size = cfg.get('sender', 'window_size')
+	window_size = int(cfg.get('sender', 'window_size'))
 	#addr, data = send_monitor.recv(max_packet_size)
-
+	print(f'window_size: {window_size}')
+	print(f'receiver_id: {type(receiver_id)}')
 	data = create_data_array(file_to_send, max_packet_size)
+	print(f'size of data: {len(data)}')
 	window_start = 0  # Start index of the window
 	window_end = window_start + window_size  # End index of the window
 	# For sliding window, we send packets in the window and wait for acks, as we receive acks, we slide the window
 	while window_start < len(data):
 		# Send packets within the window
+		if window_end > len(data):
+			window_end = len(data)
 		for i in range(window_start, window_end):
 			packet = data[i]
-			send_monitor.send(packet, receiver_id)
+			#print(f'Packet is {packet}.')
+			#print(f'type of packet is {type(packet)}.')
+			if type(packet) != bytes:
+				print(f'Packet is not bytes.')
+				packet = packet.to_bytes(4, byteorder='big')
+			elif packet == b'' or packet != None:
+				send_monitor.send(receiver_id, packet)
 
 		# Wait for acknowledgements
 		for i in range(window_start, window_end):
