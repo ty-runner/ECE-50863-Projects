67d94facd504641fd25193ee601824567309197b
ty-runner <ty.runner02@gmail.com>
2024-02-19 13:07:52 -0500

    start

--- Reliable Data Transmission Protocol/starter_code/Student Code/student/receiver.py
+++ Reliable Data Transmission Protocol/starter_code/Student Code/student/receiver.py
@@ -1,29 +1,60 @@
-#!/usr/bin/env python3
 from monitor import Monitor
 import sys
+import time
 
 # Config File
 import configparser
 
 if __name__ == '__main__':
-	print("Receivier starting up!")
-	config_path = sys.argv[1]
+    print("Receiver starting up!")
+    config_path = sys.argv[1]
 
-	# Initialize sender monitor
-	recv_monitor = Monitor(config_path, 'receiver')
-	
-	# Parse config file
-	cfg = configparser.RawConfigParser(allow_no_value=True)
-	cfg.read(config_path)
-	sender_id = int(cfg.get('sender', 'id'))
-	file_to_send = cfg.get('nodes', 'file_to_send')
-	max_packet_size = int(cfg.get('network', 'MAX_PACKET_SIZE'))
-
-	# Exchange messages!
-	addr, data = recv_monitor.recv(max_packet_size)
-	print(f'Receiver: Got message from id {addr}: {data}')
-	print('Receiver: Responding with "Hello, Sender!".')
-	recv_monitor.send(sender_id, b'Hello, Sender!')
-
-	# Exit! Make sure the receiver ends before the sender. send_end will stop the emulator.
-	recv_monitor.recv_end('received_file', sender_id)
+    # Initialize sender monitor
+    recv_monitor = Monitor(config_path, 'receiver')
+    
+    # Parse config file
+    cfg = configparser.RawConfigParser(allow_no_value=True)
+    cfg.read(config_path)
+    sender_id = int(cfg.get('sender', 'id'))
+    file_to_send = cfg.get('nodes', 'file_to_send')
+    max_packet_size = int(cfg.get('network', 'MAX_PACKET_SIZE'))
+    max_packet_size -= 12  # Account for the header size
+    write_location = cfg.get('receiver', 'write_location')
+    print('Receiver: Waiting for file contents...')
+    #listen for contents of file and sending ACKs
+    #open file to write to
+    with open(write_location, 'wb') as f:
+        received_count = 0
+        received_list = []
+        data_list = []
+		while True:
+			# Receive packet from sender
+			packet = recv_monitor.recv_packet()
+			
+			# Check if packet is the end signal
+			if packet == b'':
+				break
+			
+			# Extract sequence number and data from packet
+			seq_num = int.from_bytes(packet[:4], byteorder='big')
+			data = packet[4:]
+			
+			# Check if packet is in the window
+			if seq_num >= received_count and seq_num < received_count + max_packet_size:
+			# Check if packet has already been received
+				if seq_num not in received_list:
+					# Write data to file
+					f.write(data)
+					
+					# Update received count and list
+					received_count += 1
+					received_list.append(seq_num)
+					
+					# Send ACK to sender
+					ack_packet = seq_num.to_bytes(4, byteorder='big')
+					recv_monitor.send_packet(ack_packet)
+		
+	# Send end signal to sender
+    f.close()
+    recv_monitor.recv_end(write_location, sender_id)
+    # Exit! Make sure the receiver ends before the sender. send_end will stop the emulator.
--- Reliable Data Transmission Protocol/starter_code/Student Code/student/sender.py
+++ Reliable Data Transmission Protocol/starter_code/Student Code/student/sender.py
@@ -1,29 +1,73 @@
-#!/usr/bin/env python3
 from monitor import Monitor
 import sys
 
 # Config File
 import configparser
+import time
+import socket
+timeout = 2  # Timeout period in seconds
 
+def create_data_array(file_to_send, max_packet_size):
+	""" Creates an array of data packets from the file to send """
+	data = []
+	with open(file_to_send, 'rb') as f:
+		header_count = 0
+		while True:
+			chunk = f.read(max_packet_size)
+			if not chunk:
+				break
+			header = header_count.to_bytes(4, byteorder='big')
+			data.append(header + chunk)
+			header_count += 1
+	f.close()
+	return data
+def extract_seq_num(data):
+	""" Extracts the sequence number from the packet """
+	return int.from_bytes(data[3:7], byteorder='big')
 if __name__ == '__main__':
 	print("Sender starting up!")
 	config_path = sys.argv[1]
 
 	# Initialize sender monitor
 	send_monitor = Monitor(config_path, 'sender')
-	
+	send_monitor.socketfd.settimeout(timeout)
+	#send_monitor.socketfd.setblocking(False)
 	# Parse config file
 	cfg = configparser.RawConfigParser(allow_no_value=True)
 	cfg.read(config_path)
 	receiver_id = int(cfg.get('receiver', 'id'))
 	file_to_send = cfg.get('nodes', 'file_to_send')
 	max_packet_size = int(cfg.get('network', 'MAX_PACKET_SIZE'))
+	max_packet_size -= 12  # Account for the header size
+	window_size = cfg.get('sender', 'window_size')
+	#addr, data = send_monitor.recv(max_packet_size)
 
-	# Exchange messages!
-	print('Sender: Sending "Hello, World!" to receiver.')
-	send_monitor.send(receiver_id, b'Hello, World!')
-	addr, data = send_monitor.recv(max_packet_size)
-	print(f'Sender: Got response from id {addr}: {data}')
+	data = create_data_array(file_to_send, max_packet_size)
+	window_start = 0  # Start index of the window
+	window_end = window_start + window_size  # End index of the window
+	# For sliding window, we send packets in the window and wait for acks, as we receive acks, we slide the window
+	while window_start < len(data):
+		# Send packets within the window
+		for i in range(window_start, window_end):
+			packet = data[i]
+			send_monitor.send(packet, receiver_id)
 
+		# Wait for acknowledgements
+		for i in range(window_start, window_end):
+			try:
+				addr, data = send_monitor.recv(max_packet_size)
+			except socket.timeout:
+				print(f'Sender: Timeout occurred. Retransmitting packet...')
+			if data is not None:
+				# Process the acknowledgement
+				ack_seq_num = extract_seq_num(data)
+				if ack_seq_num == window_start:
+					# Move the window forward
+					window_start += 1
+					window_end += 1
+					break
+
+	print(f'Sender: File {file_to_send} sent to receiver.')
 	# Exit! Make sure the receiver ends before the sender. send_end will stop the emulator.
+	time.sleep(1.5)
 	send_monitor.send_end(receiver_id)
